21b893cceb22e50c934819c17ed89025291d8c08
Ashley Brown <snowedin.webdesign@gmail.com>
2017-05-30 10:53:17 -0600

    Checkpoint 9 done

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-           var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
            var revealPoint = function(num) {
                points[num].style.opacity = 1;
@@ -8,7 +8,21 @@ var animatePoints = function() {
                points[num].style.msTransform = "scaleX(1) translateY(0)";
                points[num].style.WebkitTransform = "scaleX(1) translateY(0)";
            };
-           for (var = 0; i < points.length; i++) {
+           for (var i = 0; i < points.length; i++) {
              revealPoint(i);
            }
 };
+
+window.onload = function() {
+  // Automatically animate the points on a tall screen where scrolling can't trigger the animation
+   if (window.innerHeight > 950) {
+       animatePoints(pointsArray);
+   }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
