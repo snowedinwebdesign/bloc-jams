7085ef2976d61c27d35954fdc855b142580a1200
Ashley Brown <snowedin.webdesign@gmail.com>
2017-05-30 18:28:26 -0600

    checkpoint 10 done

---
+++ scripts/collection.js
@@ -0,0 +1,24 @@
+var collectionItemTemplate =
+    '<div class="collection-album-container column fourth">'
+  +  ' <img src="assets/images/album_covers/01.png"/>'
+  +  ' <div class="collection-album-info caption">'
+  +  '   <p>'
+  +  '      <a class="album-name" href="album.html"> The Colors </a>'
+  +  '      <br/>'
+  +  '      <a href="album.html"> Pablo Picasso </a>'
+  +  '      <br/>'
+  +  '  </p>'
+  +  ' </div>'
+  +  '</div>'
+  ;
+
+  window.onload = function() {
+    // At #1, we select the first (and only, as we've designed it) element with an album-covers class name. We assign this specified element to a variable named collectionContainer.
+      var collectionContainer = document.getElementsByClassName('album-covers')[0];
+    // At #2, We assign an empty string to collectionContainer's innerHTML property to clear its content. This ensures we're working with a clean slate before we insert content with JavaScript.
+      collectionContainer.innerHTML = '';
+    // We then create a for loop, at #3, that inserts 12 albums using the += operator, which appends content to strings. Each loop adds the contents of  collectionItemTemplate (the template) to the innerHTML of  collectionContainer, thereby generating the albums that display on the collection page.
+      for (var i = 0; i < 12; i++) {
+          collectionContainer.innerHTML += collectionItemTemplate;
+      }
+  }
