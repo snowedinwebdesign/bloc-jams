eb5d109911a85da19a351507c59799e71854ef81
Ashley Brown <snowedin.webdesign@gmail.com>
2017-06-26 17:49:43 -0600

    Checkpoint 21 debugging

--- scripts/album.js
+++ scripts/album.js
@@ -25,8 +25,8 @@ var getSongNumberCell = function (number) {
 
 };
 // We declare the objects before the function because the createSongRow function uses the information stored in the album objects.
- // The createSongRow function assigns our previously static song row template to a variable named template and returns it. Instead of statically declaring the song number, name, or length, our function takes them as arguments and populates the song row template accordingly.
- var createSongRow = function(songNumber, songName, songLength) {
+// The createSongRow function assigns our previously static song row template to a variable named template and returns it. Instead of statically declaring the song number, name, or length, our function takes them as arguments and populates the song row template accordingly.
+var createSongRow = function(songNumber, songName, songLength) {
     var template =
       '<tr class="album-view-song-item">'
      + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
@@ -37,7 +37,8 @@ var getSongNumberCell = function (number) {
 
     var $row = $(template);
     var clickHandler = function() {
-	     var songNumber = parseInt($(this).attr('data-song-number'));
+
+	    var songNumber = parseInt($(this).attr('data-song-number'));
 
 	     if (currentlyPlayingSongNumber !== null) {
 		       // Revert to song number for currently playing song because user started playing new song.
@@ -50,6 +51,8 @@ var getSongNumberCell = function (number) {
              currentSoundFile.play();
              updateSeekBarWhileSongPlays();
              currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+             $(this).html(pauseButtonTemplate);
+             updatePlayerBarSong();
 
 
              var $volumeFill = $('.volume .fill');
@@ -57,9 +60,6 @@ var getSongNumberCell = function (number) {
              $volumeFill.width(currentVolume + '%');
              $volumeThumb.css({left: currentVolume + '%'});
 
-
-             $(this).html(pauseButtonTemplate);
-             updatePlayerBarSong();
 	     } else if (currentlyPlayingSongNumber === songNumber) {
           if (currentSoundFile.isPaused()) {
             $(this).html(pauseButtonTemplate);
@@ -72,18 +72,18 @@ var getSongNumberCell = function (number) {
               currentSoundFile.pause();
           }
 	     }
-   };
+  };
+  var onHover = function(event) {
 
-    var onHover = function(event) {
        var songNumberCell =$(this).find('.song-item-number');
        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
 
        if (songNumber !== currentlyPlayingSongNumber) {
            songNumberCell.html(playButtonTemplate);
        }
-   };
+  };
 
-   var offHover = function(event) {
+  var offHover = function(event) {
        var songNumberCell = $(this).find('.song-item-number');
        var songNumber = parseInt(songNumberCell.attr('data-song-number'));
 
@@ -91,13 +91,13 @@ var getSongNumberCell = function (number) {
            songNumberCell.html(songNumber);
        }
        console.log("songNumber type is " + typeof songNumber + "\n and currentlyPlayingSongNumber type is " + typeof currentlyPlayingSongNumber);
-   };
+  };
     // #1 The jQuery find() method at #1 is similar to querySelector(). We call it here to find the element with the .song-item-number class that's contained in whichever row is clicked. jQuery's click event listener executes the callback we pass to it when the target element is clicked. Notice that clickHandler() no longer takes any arguments, which we'll address in our clickHandler() refactor.
-    $row.find('.song-item-number').click(clickHandler);
+$row.find('.song-item-number').click(clickHandler);
     // #2 The hover() event listener at #2 combines the mouseover and mouseleave functions we relied on previously. The first argument is a callback that executes when the user mouses over the $row element and the second is a callback executed when the mouse leaves $row.
-    $row.hover(onHover, offHover);
+$row.hover(onHover, offHover);
     // #3 we return $row, which is created with the event listeners attached.
-    return $row;
+return $row;
  };
  var setCurrentAlbum = function(album) {
    currentAlbum = album;
@@ -144,82 +144,58 @@ var getSongNumberCell = function (number) {
  var setupSeekBars = function() {
    // #6  At #6, we are using jQuery to find all elements in the DOM with a class of "seek-bar" that are contained within the element with a class of "player-bar". This will return a jQuery wrapped array containing both the song seek control and the volume c
      var $seekBars = $('.player-bar .seek-bar');
-
      $seekBars.click(function(event) {
-  // #3 At #3, we see a new property on the event object called pageX. This is a jQuery-specific event value, which holds the X (or horizontal) coordinate at which the event occurred (think of the X-Y coordinate plane that you hated in Algebra class).
-         var offsetX = event.pageX - $(this).offset().left;
-         var barWidth = $(this).width();
-  // #4 At #4, we divide offsetX by the width of the entire bar to calculate  seekBarFillRatio.
-         var seekBarFillRatio = offsetX / barWidth;
 
-  // #5 Finally, at #5, we pass $(this) as the $seekBar argument and seekBarFillRatio for its eponymous argument to updateSeekBarPercentage().
-         updateSeekPercentage($(this), seekBarFillRatio);
-     });
-  // #7 At #7, we find elements with a class of .thumb inside our $seekBars and add an event listener for the mousedown event. A click event fires when a mouse is pressed and released quickly, but the mousedown event will fire as soon as the mouse button is pressed down. In contrast to this, the mouseup event is the opposite: it fires when the mouse button is released. jQuery allows us access to a shorthand method of attaching the mousedown event by calling mousedown on a jQuery collection.
-          $seekBars.find('.thumb').mousedown(function(event) {
-  // #8 At #8, we are taking the context of the event and wrapping it in jQuery. In this scenario, this will be equal to the .thumb node that was clicked. Because we are attaching an event to both the song seek and volume control, this is an important way for us to determine which of these nodes dispatched the event. We can then use the  parent method, which will select the immediate parent of the node. This will be whichever seek bar this .thumb belongs to.
-
-              var $seekBar = $(this).parent();
-
-  // #9 #9 introduces a new way to track events, jQuery's bind() event. bind() behaves similarly to addEventListener() in that it takes a string of an event instead of wrapping the event in a method like we've seen with all other jQuery events thus far. We use bind() because it allows us to namespace event listeners (we'll discuss namespacing, shortly). The event handler inside the bind() call is identical to the  click behavior.
-              $(document).bind('mousemove.thumb', function(event){
-                  var offsetX = event.pageX - $seekBar.offset().left;
-                  var barWidth = $seekBar.width();
-                  var seekBarFillRatio = offsetX / barWidth;
-
-                  updateSeekPercentage($seekBar, seekBarFillRatio);
-              });
-
-// #10 Finally, at #10, we bind the mouseup event with a .thumb namespace. The event handler uses the unbind() event method, which removes the previous event listeners that we just added. If we fail to unbind() them, the thumb and fill would continue to move even after the user released the mouse. Comment out this block to demonstrate the unintended behavior.
-              $(document).bind('mouseup.thumb', function() {
-                  $(document).unbind('mousemove.thumb');
-                  $(document).unbind('mouseup.thumb');
-              });
-          });
-    $seekBars.click(function(event) {
         var offsetX = event.pageX - $(this).offset().left;
         var barWidth = $(this).width();
         var seekBarFillRatio = offsetX / barWidth;
 
         if ($(this).parent().attr('class') == 'seek-control') {
-            seek(seekBarFillRatio * currentSoundFile.getDuration());
+                 seek(seekBarFillRatio * currentSoundFile.getDuration());
         } else {
-            setVolume(seekBarFillRatio * 100);
+                 setVolume(seekBarFillRatio * 100);
         }
 
         updateSeekPercentage($(this), seekBarFillRatio);
-    });
+     });
+     $seekBars.find('.thumb').mousedown(function(event) {
+         var $seekBar = $(this).parent();
 
-    $seekBars.find('.thumb').mousedown(function(event) {
+         $(document).bind('mousemove.thumb', function(event){
+           var offsetX = event.pageX - $seekBar.offset().left;
+           var barWidth = $seekBar.width();
+           var seekBarFillRatio = offsetX / barWidth;
 
-        var $seekBar = $(this).parent();
-
-        $(document).bind('mousemove.thumb', function(event){
-            var offsetX = event.pageX - $seekBar.offset().left;
-            var barWidth = $seekBar.width();
-            var seekBarFillRatio = offsetX / barWidth;
-
-            if ($seekBar.parent().attr('class') == 'seek-control') {
-                seek(seekBarFillRatio * currentSoundFile.getDuration());
+           if ($seekBar.parent().attr('class') == 'seek-control') {
+                     seek(seekBarFillRatio * currentSoundFile.getDuration());
             } else {
-                setVolume(seekBarFillRatio);
-            }
+                     setVolume(seekBarFillRatio);
+               }
 
-            updateSeekPercentage($seekBar, seekBarFillRatio);
+                 updateSeekPercentage($seekBar, seekBarFillRatio);
+         });
+  // #3 At #3, we see a new property on the event object called pageX. This is a jQuery-specific event value, which holds the X (or horizontal) coordinate at which the event occurred (think of the X-Y coordinate plane that you hated in Algebra class).
+  // #4 At #4, we divide offsetX by the width of the entire bar to calculate  seekBarFillRatio.
+  // #5 Finally, at #5, we pass $(this) as the $seekBar argument and seekBarFillRatio for its eponymous argument to updateSeekBarPercentage().
+  // #7 At #7, we find elements with a class of .thumb inside our $seekBars and add an event listener for the mousedown event. A click event fires when a mouse is pressed and released quickly, but the mousedown event will fire as soon as the mouse button is pressed down. In contrast to this, the mouseup event is the opposite: it fires when the mouse button is released. jQuery allows us access to a shorthand method of attaching the mousedown event by calling mousedown on a jQuery collection.
+  // #8 At #8, we are taking the context of the event and wrapping it in jQuery. In this scenario, this will be equal to the .thumb node that was clicked. Because we are attaching an event to both the song seek and volume control, this is an important way for us to determine which of these nodes dispatched the event. We can then use the  parent method, which will select the immediate parent of the node. This will be whichever seek bar this .thumb belongs to.
+  // #9 #9 introduces a new way to track events, jQuery's bind() event. bind() behaves similarly to addEventListener() in that it takes a string of an event instead of wrapping the event in a method like we've seen with all other jQuery events thus far. We use bind() because it allows us to namespace event listeners (we'll discuss namespacing, shortly). The event handler inside the bind() call is identical to the  click behavior.
+  // #10 Finally, at #10, we bind the mouseup event with a .thumb namespace. The event handler uses the unbind() event method, which removes the previous event listeners that we just added. If we fail to unbind() them, the thumb and fill would continue to move even after the user released the mouse. Comment out this block to demonstrate the unintended behavior.
+        $(document).bind('mouseup.thumb', function() {
+          $(document).unbind('mousemove.thumb');
+          $(document).unbind('mouseup.thumb');
         });
- });
-
- var trackIndex = function(album, song) {
+    });
+};
+var trackIndex = function(album, song) {
      return album.songs.indexOf(song);
  };
- var updatePlayerBarSong = function() {
+var updatePlayerBarSong = function() {
 
     $('.currently-playing .song-name').text(currentSongFromAlbum.title);
     $('.currently-playing .artist-name').text(currentAlbum.artist);
     $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
     $('.main-controls .play-pause').html(playerBarPauseButton);
-
-};
 var nextSong = function() {
     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
     // Note that we're _incrementing_ the song here
@@ -236,8 +212,6 @@ var nextSong = function() {
     setSong(currentSongIndex + 1);
     currentSoundFile.play();
     updateSeekBarWhileSongPlays();
-
-
     // Update the Player Bar information
     updatePlayerBarSong();
 
@@ -255,18 +229,14 @@ var previousSong = function() {
     if (currentSongIndex < 0) {
         currentSongIndex = currentAlbum.songs.length - 1;
     }
-
     // Save the last song number before changing it
     var lastSongNumber = currentlyPlayingSongNumber;
-
     // Set a new current song
     setSong(currentSongIndex + 1);
     currentSoundFile.play();
     updateSeekBarWhileSongPlays();
-
     // Update the Player Bar information
     updatePlayerBarSong();
-
     $('.main-controls .play-pause').html(playerBarPauseButton);
 
     var $previousSongNumberCell = getSongNumberCell();
@@ -275,8 +245,6 @@ var previousSong = function() {
     $previousSongNumberCell.html(pauseButtonTemplate);
     $lastSongNumberCell.html(lastSongNumber);
 };
-
-
  // Album button templates
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
@@ -296,22 +264,9 @@ var previousSong = function() {
  var $previousButton = $('.main-controls .previous');
  var $nextButton = $('.main-controls .next');
 
-
-
-<<<<<<< HEAD
-
  $(document).ready(function() {
-      setCurrentAlbum(albumPicasso);
-      $previousButton.click(previousSong);
-      $nextButton.click(nextSong);
-
-
-  });
-=======
-$(document).ready(function() {
-    setCurrentAlbum(albumPicasso);
-    setupSeekBars();
-    $previousButton.click(previousSong);
-    $nextButton.click(nextSong);
-});
->>>>>>> checkpoint-21-JQUERY
+  setCurrentAlbum(albumPicasso);
+  setupSeekBars();
+  $previousButton.click(previousSong);
+  $nextButton.click(nextSong);
+})};
